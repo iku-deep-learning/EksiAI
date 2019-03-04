245b03ed280d309ad0fd975517add610dc3b5c89
Ozan Yurtsever <ozanyurtsever95@gmail.com>
2019-03-04 16:19:12 +0300

    Back slashes are removed from the entry strings

--- Scrapy Model/spiders/entry_getter.py
+++ Scrapy Model/spiders/entry_getter.py
@@ -1,47 +1,57 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class EntryGetterSpider(scrapy.Spider):
     name = 'entry_getter'
     allowed_domains = ['eksisozluk.com/entry/2']
 
-    start_urls = ['http://eksisozluk.com/entry/%s' % i for i in range(1,10000)]
+    start_urls = ['http://eksisozluk.com/entry/%s' % i for i in range(1,150)]
+    #start_urls = ['http://eksisozluk.com/entry/15']
 
     def parse(self, response):
 
         topic = response.xpath("//span[@itemprop='name']/text()").extract()
         topic = str(topic)
-        # only first second to last 2nd element contains valid author name
         topic = topic[2:len(topic)-2]
-        
-        # if we extract text() only we loose links within the entry
+
+
         entry = response.xpath("//div[@class='content']").extract()
         entry = str(entry)
-        # only first 31st to last 15th element contains valid author name
+        if "class=\"b\"" in entry:
+            entry = entry.replace("class=\"b\"", "")
+        c = '\\'
+        position = [pos for pos, char in enumerate(entry) if char == c]
+        entry_list = list(entry)
+        for index in position:
+            entry_list[index] = 'A'
+        entry = "".join(entry_list)
         entry = entry[31:len(entry)-15]
-        
+
         author = response.xpath("//a[@class='entry-author']/text()").extract()
         author = str(author)
-        # only first second to last 2nd element contains valid author name
         author = author[2:len(author) - 2]
 
         # this returns a list e.g [/entry/2]
         entry_id = response.xpath("//a[@class='entry-date permalink']/@href").extract()
         # conversion to string '[/entry/2]'
         entry_id = str(entry_id)
-        # only last 3rd element contains the real entry id
-        entry_id = entry_id[len(entry_id)-3]
-        
+        # only gets the digit value from /entry/xyzclc
+        entry_id = str(re.search(r'\d+', entry_id).group(0))
+
         entry_date = response.xpath("//a[@class='entry-date permalink']/text()").extract()
         entry_date = str(entry_date)
         entry_date = entry_date[2:len(entry_date)-2]
-        
+
+
+
         yield {
-            
+
             "entryId":entry_id,
             "author": author,
             "topic": topic,
-            "entry": entry
+            "entry": entry,
+            "entryDate": entry_date
 
         }
