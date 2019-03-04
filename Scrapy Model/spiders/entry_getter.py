5aa5ae86f50bf171d8d382d1b78af2f79a9397f6
Soner Abay <11530785+SonerAbay@users.noreply.github.com>
2019-03-04 07:39:33 +0300

    Update entry_getter.py

--- Scrapy Model/spiders/entry_getter.py
+++ Scrapy Model/spiders/entry_getter.py
@@ -12,20 +12,24 @@ class EntryGetterSpider(scrapy.Spider):
 
         topic = response.xpath("//span[@itemprop='name']/text()").extract()
         topic = str(topic)
+        # only first second to last 2nd element contains valid author name
         topic = topic[2:len(topic)-2]
         
         entry = response.xpath("//div[@class='content']/text()").extract()
         entry = str(entry)
+        # only first 10th to last 8th element contains valid author name
+        entry = entry[10:len(entry)-8]
         
         author = response.xpath("//a[@class='entry-author']/text()").extract()
         author = str(author)
+        # only first second to last 2nd element contains valid author name
         author = author[2:len(author) - 2]
 
         # this returns a list e.g [/entry/2]
         entry_id = response.xpath("//a[@class='entry-date permalink']/@href").extract()
         # conversion to string '[/entry/2]'
         entry_id = str(entry_id)
-        # only last 3rd element containts the real entry id
+        # only last 3rd element contains the real entry id
         entry_id = entry_id[len(entry_id)-3]
         
         yield {
