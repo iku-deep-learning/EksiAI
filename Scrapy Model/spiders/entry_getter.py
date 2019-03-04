7078dc1d6f8f2aaf15345f7dfc3107bad7da7390
Soner Abay <11530785+SonerAbay@users.noreply.github.com>
2019-03-04 07:32:21 +0300

    Update entry_getter.py

--- Scrapy Model/spiders/entry_getter.py
+++ Scrapy Model/spiders/entry_getter.py
@@ -11,10 +11,27 @@ class EntryGetterSpider(scrapy.Spider):
     def parse(self, response):
 
         topic = response.xpath("//span[@itemprop='name']/text()").extract()
+        topic = str(topic)
+        topic = topic[2:len(topic)-2]
+        
         entry = response.xpath("//div[@class='content']/text()").extract()
+        entry = str(entry)
+        
+        author = response.xpath("//a[@class='entry-author']/text()").extract()
+        author = str(author)
+        author = author[2:len(author) - 2]
 
+        # this returns a list e.g [/entry/2]
+        entry_id = response.xpath("//a[@class='entry-date permalink']/@href").extract()
+        # conversion to string '[/entry/2]'
+        entry_id = str(entry_id)
+        # only last 3rd element containts the real entry id
+        entry_id = entry_id[len(entry_id)-3]
+        
         yield {
-
+            
+            "entryId":entry_id,
+            "author": author,
             "topic": topic,
             "entry": entry
 
